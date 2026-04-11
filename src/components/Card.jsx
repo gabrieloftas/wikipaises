334451618b9e0d1591685e75e42bb2237d4e91ff
Gabrieloftas <gabrieloftas@outlook.com>
2026-04-11 17:09:20 -0300

    feat: Header componente

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,16 @@
 function Card(){
     return(
     <>
-        
+        <div className="explore">
+
+        </div>
+        <div className="cards">
+            <div className="countries">
+                
+            </div>
+        </div>
     </>
     );
 }
+
+export default Card;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
+import './Header.css';
+
 function Header(){
     return(
     <>
-        
+        <div className="header">
+            <h1>WikiPaíses</h1>
+            <nav className="navbar">
+                <a href="/">Europa</a>
+                <a href="/">América</a>
+                <a href="/">Ásia</a>
+                <a href="/">África</a>
+                <a href="/">Oceania</a>
+            </nav>
+
+        </div>
     </>
     );
 }
+
+export default Header;
